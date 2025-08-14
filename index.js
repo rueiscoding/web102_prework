d0a363031c855298997d5a6d98f6a25876cbebcc
rueiscoding <snixorstix@gmail.com>
2025-08-14 17:33:10 -0400

    completed challenge 4

--- index.js
+++ index.js
@@ -72,19 +72,22 @@ addGamesToPage(GAMES_JSON);
 const contributionsCard = document.getElementById("num-contributions");
 
 // use reduce() to count the number of total contributions by summing the backers
-
+const total_backers = GAMES_JSON.reduce( (acc, game) =>{return acc + game.backers}, 0);
 
 // set the inner HTML using a template literal and toLocaleString to get a number with commas
-
+contributionsCard.innerHTML = `<div>${total_backers.toLocaleString('en-US')}</div>`;
 
 // grab the amount raised card, then use reduce() to find the total amount raised
 const raisedCard = document.getElementById("total-raised");
+const total_pledged = GAMES_JSON.reduce( (acc, game) =>{return acc + game.pledged}, 0);
 
 // set inner HTML using template literal
-
+raisedCard.innerHTML = `<div>$${total_pledged.toLocaleString('en-US')}</div>`;
 
 // grab number of games card and set its inner HTML
 const gamesCard = document.getElementById("num-games");
+const number_of_games = GAMES_JSON.length;
+gamesCard.innerHTML =  `<div>${number_of_games}</div>`
 
 
 /*************************************************************************************
